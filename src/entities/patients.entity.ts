7342dd6eb88f44091fbeda1e29c7f1f8fd9429dc
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-02-17 16:58:20 +0800

    feat(Patient): add patient model

---
+++ src/entities/patients.entity.ts
@@ -0,0 +1,40 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Schema as MongooseSchema } from "mongoose";
+
+export enum Gender {
+    MALE = 'male',
+    FEMALE = 'female'
+}
+
+@Schema({ timestamps: true })
+export class Patient extends Document {
+    @Prop({
+        type: MongooseSchema.Types.ObjectId,
+        default: MongooseSchema.Types.ObjectId,
+        unique: true
+    })
+    patient_id: string;
+
+    @Prop({ required: true })
+    first_name: string;
+
+    @Prop({ required: true })
+    last_name: string;
+
+    @Prop({ required: true })
+    dob: Date;
+
+    @Prop({ required: true, enum: Gender })
+    gender: Gender;
+
+    @Prop({ required: true })
+    contact_number: string;
+
+    @Prop({ required: true })
+    email: string;
+
+    @Prop({ required: true })
+    address: string;
+}
+
+export const PatientSchema = SchemaFactory.createForClass(Patient)
