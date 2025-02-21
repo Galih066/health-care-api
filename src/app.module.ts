16ea53bd08a47674975033391ad0400ccac5ffae
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-02-21 17:22:05 +0800

    feat(Patient): register patient and profile to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PatientModule } from './module/patient/patient.module';
+import { ProfileModule } from './module/profile/profile.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
-		MongooseModule.forRoot(process.env.MONGO_URL, { dbName: process.env.DB_NAME })
+		MongooseModule.forRoot(process.env.MONGO_URL, { dbName: process.env.DB_NAME }),
+		PatientModule,
+		ProfileModule
 	],
 	controllers: [AppController],
 	providers: [AppService],
