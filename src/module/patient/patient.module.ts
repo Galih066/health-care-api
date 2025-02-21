1c8f88975fce6a8f2543006f207c2fd00ba030ad
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-02-21 17:39:40 +0800

    feat(Patient): register patient entity to patient module

--- src/module/patient/patient.module.ts
+++ src/module/patient/patient.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PatientService } from './patient.service';
 import { PatientController } from './patient.controller';
+import { Patient } from 'src/entities/patients.entity';
+import { PatientSchema } from 'src/entities/patients.entity';
 
 @Module({
-  controllers: [PatientController],
-  providers: [PatientService],
+	imports: [
+		MongooseModule.forFeature([
+			{ name: Patient.name, schema: PatientSchema },
+		])
+	],
+	controllers: [PatientController],
+	providers: [PatientService],
 })
-export class PatientModule {}
+export class PatientModule { }
