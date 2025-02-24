3ae5e956017268cecefd673414bafe18188346b8
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-02-24 17:17:33 +0800

    docs(Auth): add swagger documentation field for sign up

--- src/module/auth/dto/sign-up.dto.ts
+++ src/module/auth/dto/sign-up.dto.ts
@@ -1,10 +1,13 @@
 import { IsEmail, IsNotEmpty } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class SignUpDto {
+    @ApiProperty({ required: true, default: 'example@mail.com' })
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({ required: true, default: 'user123', minLength: 8 })
     @IsNotEmpty()
     password: string;
 }
