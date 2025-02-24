a72888c9684f50b2b8b716abbf61b1c59b6ed406
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-02-24 17:07:10 +0800

    feat(Docs): setup for swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+	const config = new DocumentBuilder()
+		.setTitle('Health Care Lite API')
+		.setDescription('This is API for health care functionality')
+		.addTag('health', 'healthy')
+		.build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, documentFactory);
 	await app.listen(process.env.PORT ?? 3000);
 }
 
